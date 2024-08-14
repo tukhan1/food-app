8d72337bb1f4564e5f8d31ac1516b4726fe09b41
Egor <tushev.ea@yandex.com>
2024-08-14 17:16:39 +0300

    Initial Commit

---
+++ food-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  food-app
+//
+//  Created by Egor Tushev on 14.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ food-app/food_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  food_appApp.swift
+//  food-app
+//
+//  Created by Egor Tushev on 14.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct food_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
